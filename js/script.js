1df422bcccf26d7636615dbbaf7560674bb559bd
Nike <198646766+BlakKMango@users.noreply.github.com>
2025-12-02 09:34:05 +0000

    Fix addSquaresToCanvas()
    The function now works as intended:
    - border-box added to CSS to the boxes fit within the parent container
    - create.Element added inside the for loop to ensure the correct number of elements are added each time
    - dynamic box sizing has been added using only flex-box (as per the project specification)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const canvas = document.querySelector("#canvas");
 
 function addSquaresToCanvas(userInputNum){
-    totalSquares = userInputNum * userInputNum;
-    for(let i = 0; i <= totalSquares; i++){
-        const pixel = document.createElement("div")
-        pixel.classList.add = "rendered"
-        canvas.appendChild(pixel)
+    let totalSquares = userInputNum * userInputNum;
+    let squareSize = (800/userInputNum)
+    let squareSizeString = squareSize.toString() + "px";
+    
+    for(let i = 0; i < totalSquares; i++){
+        let pixel = document.createElement("div")
+        pixel.classList.add("rendered");
+        pixel.style.minWidth = squareSizeString
+        pixel.style.minHeight = squareSizeString
+        canvas.appendChild(pixel);
     }
 }
 
-addSquaresToCanvas(10)
+addSquaresToCanvas(6)
