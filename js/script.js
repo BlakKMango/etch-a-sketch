c44c81b81812bd1163f92931786681790a7df4b1
Nike <198646766+BlakKMango@users.noreply.github.com>
2025-12-02 08:21:49 +0000

    Append new divs to the canvas

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const canvas = document.querySelector("#canvas");
 function addSquaresToCanvas(userInputNum){
     totalSquares = userInputNum * userInputNum;
     for(let i = 0; i <= totalSquares; i++){
-        const pixel = canvas.createElement("div")
+        const pixel = document.createElement("div")
         pixel.classList.add = "rendered"
+        canvas.appendChild(pixel)
     }
 }
+
+addSquaresToCanvas(10)
